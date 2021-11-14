433e014fcdcf9fc7082359400115d4fb3e66075a
NohJangHyuk <nohriter@gmail.com>
2021-11-14 17:28:39 +0900

    게시판 crud 기능 완성

--- src/main/java/com/practice/oauthlogin/web/IndexController.java
+++ src/main/java/com/practice/oauthlogin/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.practice.oauthlogin.web;
 
 import com.practice.oauthlogin.service.PostsService;
+import com.practice.oauthlogin.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,5 +24,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
