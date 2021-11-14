dddb7150b92de4db04c295418f652f57c8234fd8
NohJangHyuk <nohriter@gmail.com>
2021-11-14 16:35:17 +0900

    글 수정, 찾기 기능 추가

---
+++ src/main/java/com/practice/oauthlogin/service/PostsService.java
@@ -0,0 +1,38 @@
+package com.practice.oauthlogin.service;
+
+import com.practice.oauthlogin.domain.posts.Posts;
+import com.practice.oauthlogin.domain.posts.PostsRepository;
+import com.practice.oauthlogin.web.dto.PostsResponseDto;
+import com.practice.oauthlogin.web.dto.PostsSaveRequestDto;
+import com.practice.oauthlogin.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ src/main/java/com/practice/oauthlogin/web/PostsApiController.java
@@ -0,0 +1,35 @@
+package com.practice.oauthlogin.web;
+
+import com.practice.oauthlogin.service.PostsService;
+import com.practice.oauthlogin.web.dto.PostsResponseDto;
+import com.practice.oauthlogin.web.dto.PostsSaveRequestDto;
+import com.practice.oauthlogin.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
